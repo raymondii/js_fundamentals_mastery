366b36b54cc8a6f6eac0245c0d7f175473063a95
Ray Dixon <ray@Rays-MacBook-Air.local>
2024-01-08 19:18:42 -0500

    redone

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let questions = [
+const HIDE_CLASS = "hide";
+const SHOW_CLASS = "show";
+
+const questions = [
     {
         questionText: "What does JS stand for?",
         choices: ["Just Sitting", "Just Saying", "Just Standing", "JavaScript"],
@@ -21,122 +24,110 @@ let questions = [
     }
 ];
 
-let choicesDiv = document.querySelector(".choices");
-let startbutton = document.querySelector("#start");
-let startWrap = document.querySelector(".start-wrap");
-let questionWrap = document.querySelector(".question-wrap");
-let timeOutput = document.querySelector("#time-output");
-let scoreWarp = document.querySelector(".score-wrap");
-let savebutton = document.querySelector("#save-score");
+const choicesDiv = document.querySelector(".choices");
+const startButton = document.querySelector("#start");
+const startWrap = document.querySelector(".start-wrap");
+const questionWrap = document.querySelector(".question-wrap");
+const timeOutput = document.querySelector("#time-output");
+const scoreWrap = document.querySelector(".score-wrap");
+const saveButton = document.querySelector("#save-score");
+
 let questionIndex = 0;
 let time = 60;
 let timer;
 let clicked = false;
 
-if (questionIndex === questionIndex) {
-    console.log("End the Game");
+function endGame() {
+    clearInterval(timer);
+    questionWrap.classList.add(HIDE_CLASS);
+    const scoreOutput = document.querySelector("#score-output");
+    scoreOutput.innerText = "Score: " + time;
+    scoreWrap.classList.remove(HIDE_CLASS);
 }
 
 function checkAnswer(eventObj) {
     eventObj.stopPropagation();
-    if (clicked) {
-        return;
-    }
+    if (clicked) return;
 
-    let el = eventObj.target;
-    let currentQuestionObj = questions[questionIndex];
+    const el = eventObj.target;
+    const currentQuestionObj = questions[questionIndex];
 
     if (el.tagName === "BUTTON") {
-        let userAnswer = el.innerText;
-        let answerAlert = document.querySelector(".answer-alert")
+        const userAnswer = el.innerText;
+        const answerAlert = document.querySelector(".answer-alert");
 
         if (userAnswer === currentQuestionObj.correctAnswer) {
             answerAlert.innerText = "Correct";
-            answerAlert.classList.add("show");
         } else {
             answerAlert.innerText = "Wrong";
-            answerAlert.classList.add("Show");
-            time = (time - 15) < 0 ? 0 : time - 15;
+            time = Math.max(0, time - 15);
         }
 
+        answerAlert.classList.add(SHOW_CLASS);
         clicked = true;
 
-        setTimeout(function() {
-            answerAlert.classList.remove("show");
-            questionIndex++
-            if (questionIndex === questions.length) {
+        setTimeout(function () {
+            answerAlert.classList.remove(SHOW_CLASS);
+            questionIndex++;
+            if (questionIndex >= questions.length) {
                 endGame();
             } else {
                 displayQuestion();
                 clicked = false;
             }
-        }, 1500)
-    }
-
-
-};
-
-function displayQuestion() {
-    let currentQuestionObj = questions[questionIndex];
-
-    let textEl = document.querySelector(".question-text")
-    choicesDiv.innerHTML = "";
-    textEl.innerText = currentQuestionObj.questionText;
-    for (let i = 0; i < currentQuestionObj.choices.length; i++) {
-        let choiceBtn = document.createElement("button");
-        choiceBtn.innerText = currentQuestionObj.choices[i];
-        let choicesDiv = document.querySelector(".choices");
-        choicesDiv.append(choiceBtn);
-        
-
+        }, 1500);
     }
 }
 
+function displayQuestion() {
+    const currentQuestionObj = questions[questionIndex];
+
+    const textEl = document.querySelector(".question-text");
+    choicesDiv.innerHTML = "";
+    textEl.innerText = currentQuestionObj.questionText;
+
+    currentQuestionObj.choices.forEach(choice => {
+        const choiceBtn = document.createElement("button");
+        choiceBtn.innerText = choice;
+        choicesDiv.append(choiceBtn);
+    });
+}
 
 function startCountdown() {
-    timeOutput.innerText = "Time Left :" + time;
-    timer = setInterval(function() {
-        time = (time - 1) < 0 ? 0 : time - 1;
-        timeOutput.innerText = "Time Left :" + time;
+    timeOutput.innerText = "Time Left: " + time;
+    timer = setInterval(function () {
+        time = Math.max(0, time - 1);
+        timeOutput.innerText = "Time Left: " + time;
         if (time <= 0) {
             endGame();
         }
     }, 1000);
-
 }
 
 function saveScore() {
-    let initialInput = document.querySelector("#initial-input");
-    let initials = initialInput.value
-    let rawData = localStorage.getItem("highscores");
-    let highscores = JSON.parse(rawData) || [];
-    
+    const initialInput = document.querySelector("#initial-input");
+    const initials = initialInput.value;
+    const rawData = localStorage.getItem("highscores");
+    const highscores = JSON.parse(rawData) || [];
+
     highscores.push({
         initials: initials,
         score: time
-    })
+    });
+
     localStorage.setItem("highscores", JSON.stringify(highscores));
     window.location = "./highscores.html";
-
 }
 
 function startQuiz() {
     time = 60;
     questionIndex = 0;
-    startWrap.classList.add("hide");
-    questionWrap.classList.remove("hide");
+    startWrap.classList.add(HIDE_CLASS);
+    questionWrap.classList.remove(HIDE_CLASS);
     displayQuestion();
     startCountdown();
 }
 
-function endGame() {
-    clearInterval(timer);
-    questionWrap.classList.add("hide");
-    let scoreOutput = document.querySelector("#score-output");
-    scoreOutput.innerText = "Score: " + time;
-    scoreWarp.classList.remove("hide");
-}
-
 choicesDiv.addEventListener("click", checkAnswer);
-startbutton.addEventListener("click", startQuiz)
-savebutton.addEventListener("click", saveScore);
+startButton.addEventListener("click", startQuiz);
+saveButton.addEventListener("click", saveScore);
